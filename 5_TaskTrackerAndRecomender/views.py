f00a3261587f05f6b9a2a23cc12a72aa4cebc399
Avishka999 <avishkapriyashan6@gmail.com>
2022-03-16 11:01:23 +0530

    Create views.py

---
+++ 5_TaskTrackerAndRecomender/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
+
+import datetime
+from datetime import date
+
+# Returns the current local date
+today = date.today()
+print(today)
+
+end_date = today + datetime.timedelta(days=0)
+print(end_date)
+
+def messages():
+    if end_date > today:
+         messages.success("COMPLETE THE TASK")
+         return HttpResponseRedirect() #////
+        
