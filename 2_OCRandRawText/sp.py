66b38cc2d94e6389a08313d2443f2b0b2cc0ab1c
YOMANHelloworld <80374913+YOMANHelloworld@users.noreply.github.com>
2022-04-20 21:53:10 +0530

    Add files via upload

---
+++ 2_OCRandRawText/sp.py
@@ -0,0 +1,27 @@
+def Separatetxt(filename):
+   f = open(filename)
+   
+   str1 = ""
+   str2 = ""
+   str3 = ""
+   
+   for x, line in enumerate(f):
+         if  x == 14 :
+            str1 = line
+            a = open('D:\campus\\text\Task_1.txt', 'w')
+            a.write(str1)
+            a.close()
+
+         if x == 18 :
+            str2 = line
+            b = open('D:\campus\\text\Task_2.txt', 'w')
+            b.write(str2)
+            b.close()
+
+         if x == 24:
+            str3 = line
+            c = open('D:\campus\\text\Task_3.txt', 'w')
+            c.write(str3)
+            c.close()
+         
+   f.close()
