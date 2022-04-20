1db810b0047e4b06a6556ab39c001f005243ae62
YOMANHelloworld <80374913+YOMANHelloworld@users.noreply.github.com>
2022-04-20 21:52:32 +0530

    Update searchtxt.py

--- 2_OCRandRawText/searchtxt.py
+++ 2_OCRandRawText/searchtxt.py
@@ -1,8 +1,14 @@
 def searchtxt():
     import os
     import seperatetxt
+    import sp
 
     for root,dirs,files in os.walk('D:\campus\ex'):
         for file in files:
                 if file.endswith('.txt'):
-                    seperatetxt.Separatetxt(os.path.join(root,file))
+                    if(os.path.join(root,file) == 'D:\campus\ex\pimg.txt'):
+                        seperatetxt.Separatetxt(os.path.join(root,file))
+                        print(os.path.join(root,file))
+                    elif(os.path.join(root,file) == 'D:\campus\ex\pdoc.txt'):
+                        sp.Separatetxt(os.path.join(root,file))
+                        print(os.path.join(root,file))
