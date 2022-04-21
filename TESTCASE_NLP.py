29868441e45e3cd91c2c3cd3db4178c7b6fb1a8b
Dieuzu <dieuzu@null.net>
2022-04-22 00:55:23 +0530

    Updated NLP code + added Standalone test case

--- TESTCASE_NLP.py
+++ TESTCASE_NLP.py
@@ -1,7 +1,7 @@
 from researchGap import NLPFindKeyword
 
 print ("\n===================================== NLP Keyword Extraction =====================================\n")
-directory = "3_NLPandKeyword\\Sample_Text\\" # this is simulation folder
+directory = "TestCaseCodes\\Rishi\\" # this is simulation folder
 taskcount = 0
 taskName = "Task_"
 
