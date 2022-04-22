d81454bb84156d87d867272cba6d5b2172cc3ec3
YOMANHelloworld <vanujaj@gmail.com>
2022-04-22 13:45:34 +0530

    New changes

---
+++ researchGap/readImage.py
@@ -0,0 +1,14 @@
+def readImage(imagename):
+    print("------------------------------------- Image To Text Extraction Start Here -------------------------------------\n")
+    import pytesseract as tess
+    tess.pytesseract.tesseract_cmd = r'Tesseract-OCR\\tesseract.exe'
+    from PIL import Image
+
+    img = Image.open(imagename)
+    print("[SYSTEM] Extracting data from image to text\n")
+    text = tess.image_to_string(img)
+    print("[SYSTEM] Writing data to a text file\n")
+
+    f = open('input\\pimg.txt', 'w')
+    f.write(text)
+    f.close()
---
+++ researchGap/searchtxt.py
@@ -0,0 +1,17 @@
+def searchtxt():
+    import os
+    import seperatetxt
+    import sp
+    print("------------------------------------- Getting The Txt File And Seperate Task -------------------------------------\n")
+    for root,dirs,files in os.walk('input\\'):
+        for file in files:
+                if file.endswith('.txt'):
+                    if(os.path.join(root,file) == 'input\pimg.txt'):
+                        print("[SYSTEM] Found name called "+ os.path.join(root,file)+"\n")
+                        seperatetxt.Separatetxt(os.path.join(root,file))
+                        
+                    elif(os.path.join(root,file) == 'input\pdoc.txt'):
+                        print("[SYSTEM] Found name called "+ os.path.join(root,file)+"\n")
+                        sp.Separatetxt(os.path.join(root,file))
+                        
+                
